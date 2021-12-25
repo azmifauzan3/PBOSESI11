7a376d6cb3a96cd69e17295cd5cc29bf605321a3
azmifauzan3 <81280706+azmifauzan3@users.noreply.github.com>
2021-12-25 08:39:52 +0700

    Add files via upload

---
+++ KotakBelanja.java
@@ -0,0 +1,77 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MUHAMMAD AZMI FAUZAN
+ */
+public class KotakBelanja {
+    private int maxAmount = 0;
+  private int currentIndex = 0;
+  private String[] items = new String[1];
+
+  void setMaxItemAmount(int maxAmount) {
+    this.maxAmount = maxAmount;
+    this.items = new String[this.maxAmount];
+  }
+
+  void addItem(String item) throws ArrayIndexOutOfBoundsException {
+    this.items[this.currentIndex] = item;
+    this.currentIndex++;
+  }
+
+  String[] getItems() {
+    return this.items;
+  }
+    public static void main(String[] args) {
+    KotakBelanja kotakBelanja = new KotakBelanja();
+    Scanner jumlahItemHandler = new Scanner(System.in);
+    Scanner productInput = new Scanner(System.in);
+    Scanner statusInput = new Scanner(System.in);
+    boolean addingProduct = true;
+
+    System.out.print("Mau muat berapa produk ? : ");
+    int jumlahMaksimalProduk = jumlahItemHandler.nextInt();
+    kotakBelanja.setMaxItemAmount(jumlahMaksimalProduk);
+    System.out.println(jumlahMaksimalProduk);
+
+    try {
+      while(addingProduct) {
+        System.out.println();
+        System.out.println("=======================");
+        System.out.println("[1] Tambah produk ");
+        System.out.println("[2] Keluar ");
+        System.out.println("=======================");
+        System.out.println();
+
+        System.out.print("Pilih : ");
+        int decision = statusInput.nextInt();
+
+        if (decision == 1) {
+          System.out.print("Masukan nama produk      : ");
+          String produk = productInput.nextLine();
+          kotakBelanja.addItem(produk);
+
+          System.out.println("PRODUK DITAMBAHKAN");
+        }
+
+        if (decision == 2) {
+          addingProduct = false;
+        }
+      }
+
+    } catch (ArrayIndexOutOfBoundsException err) {
+      System.out.println("Kotak belanja anda sudah penuh");
+    } finally {
+      System.out.println("Isi kotak belanja anda : " + Arrays.deepToString(kotakBelanja.getItems()));
+        }
+    
+    }
+}
